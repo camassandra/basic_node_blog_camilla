686587620bdd89cc46d53f7475732de21096de31
Camassandra <pontecamilla@yahoo.com>
2017-02-28 18:06:15 +0100

    one user can post and comment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var session = require('express-session');
 var bodyParser = require('body-parser');
 
-//const Db = require(__dirname +'/db_module.js')
+const Db = require(__dirname +'/db_module.js')
 
 //here comes the middleware stuff!
 
@@ -17,11 +17,68 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set('views', __dirname+'/views');
 app.set('view engine', 'pug');
 
-
+//homepage 
 app.get('/', function(req, response) {
+	console.log('rendering homepage')
 	response.render('index')
 });
+//about page 
+app.get('/about', function(req, response){
+	console.log('rendering abt page')
+	response.render('about')
+});
 
+//
+//registration page
+app.get('/register', function(req, response){
+	console.log('rendering registration page')
+	response.render('newuser')
+});
+//registering a new user
+app.post('/', function(req, response){
+	console.log('handling new user request')
+
+})
+
+
+//render login page
+app.get('/userlogin', function(req, response){
+	console.log('rendering login page')
+	response.render('login')
+
+});
+
+//just trying out if user session post request works 
+app.get('/usersession', function(req, response){
+	console.log('rendering usersession page')
+	Db.Posts.findAll()
+    .then((allPosts) => {
+        console.log('allPosts')
+      
+        response.render('usersession', {posts: allPosts})
+    })  
+});
+
+
+app.post('/loginhandler', function(req, response){
+
+	response.render('/usersession')
+})
+
+//posting new philosophy 
+app.post('/posthandler', function(req, response){
+	var titlepostqr = req.body.titlepostqr
+	console.log(titlepostqr)
+ Db.Posts.create({
+ 	title: titlepostqr,
+ 	body: req.body.bodypostqr
+ }).then( f => {
+ 	response.redirect('/usersession')
+ 			})
+
+});
+
+//COMMENT A POST
 
 app.listen(3000, function(){
 	console.log('listening on 3000 has started');
--- db_module.js
+++ db_module.js
@@ -1,28 +1,96 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize ('postgres://postgres:' + process.env.POSTGRES_PASSWORD + '@localhost/nodeblog');
-const Messages = db.define('messages', {
+
+const Posts = db.define('posts', {
 	title: {
+	    type: Sequelize.STRING,
+	    allowNull: false
+    },
+	body: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+})
+
+const Comments = db.define('comments', {
+	comment_author: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+    email_author: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+    comment: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+})
+const Users = db.define('users', {
+	firstname: {
       type: Sequelize.STRING,
       allowNull: false
     },
-	body: {
-	type: Sequelize.STRING,
-	allowNull: false
-	}
+	secondname: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	username: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	password: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	email: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
 }) 
 db.sync({
 	force: true
 })
+.then(function(userparameters){
+	const userone = {
+		firstname: 'Trumping',
+		secondname: 'Dummy',
+		username: 'dummyuser',
+		password: '1234',
+		email: 'dummyTrump@yahoo.net'	
+	}
+
+	Users.create(userone)
+})
+.catch((error) => console.log(error))
+
 .then(function(postparameters){
 	const postOne = {
-		title: 'rainy days',
-		body: 'how I hate rainy days, all is gloomy',
+		title: 'Unbearable',
+		body: 'How I hate rainy days, all is gloomy',
 	}
 
 	const postTwo = {
-		title: 'sunny days',
-		body: 'I love Amsterdam on sunny days!',
+		title: 'Smiling and Suffering',
+		body: 'This is the teaching of the Buddha: You will not be punished for your anger, you will be punished by your anger.',
 	}
 
 	const postThree = {
-		title: 'good advice Maori',
+		title: 'The moment',
+		body: 'Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.',
+	}
+	Posts.create(postOne)
+	Posts.create(postTwo)
+	Posts.create(postThree)
+})
+.catch((error) => console.log(error));
+
+Comments.belongsTo(Posts); //adds Posts ID to Comments
+Posts.hasMany(Comments, {as: 'postID'}); //adds PostsID to Comments
+Users.hasMany(Posts, {as: 'userID'}); //adds UsersID to Posts
+
+module.exports = {
+	Db: db,
+	Posts: Posts,
+	Users: Users
+}
